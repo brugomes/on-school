535c1865dd345b64fed3cedae49f4118c3f91a06
Viviani Rodrigues <viviani.rodrigues@elo.com.br>
2024-07-29 17:51:18 -0300

    Alteracao teste unitario

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "ts"
-  ],
-  moduleNameMapper: {  
-    "@/(.*)": "<rootDir>/$1",
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/$1',
   },
-  rootDir: "src",
+  rootDir: 'src',
   transform: {
-    "^.+\\.(t|j)s$": "ts-jest"
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: [
-    "**/*.(t|j)s"
-  ],
-  coverageDirectory: "../coverage",
-  testEnvironment: "node"
- };
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
--- src/repositories/typeorm/__mocks__/post.respository.ts
+++ src/repositories/typeorm/__mocks__/post.respository.ts
@@ -7,62 +7,62 @@ import { User } from '@/entities/user.entity'
 import { hash } from 'bcryptjs'
 
 export class PostRepository {
-
-  constructor() {
-  }
+  constructor() {}
 
   async create(post: IPost): Promise<IPost> {
-    const hashedPassword = await hash("123", 8)
+    const hashedPassword = await hash('123', 8)
     return {
-      title: "titulo",
-      content: "content",
+      title: 'titulo',
+      content: 'content',
       author: {
-        name: "Usuario",
-        email: "123",
+        name: 'Usuario',
+        email: '123',
         password: hashedPassword,
         isadmin: true,
         posts: [],
-        createdAt: new Date()
-      }
+        createdAt: new Date(),
+      },
     }
   }
 
   async findById(id: string): Promise<IPost | undefined> {
-    const hashedPassword = await hash("123", 8)
+    const hashedPassword = await hash('123', 8)
     const posts = [
       {
-        id: "1",
-        title: "titulo",
-        content: "content",
+        id: '1',
+        title: 'titulo',
+        content: 'content',
         author: {
-          name: "Usuario",
-          email: "123",
+          name: 'Usuario',
+          email: '123',
           password: hashedPassword,
           isadmin: true,
           posts: [],
-          createdAt: new Date()
-        }
-      }
-    ] 
-    return posts.find((post)=>{
-      return post.id === id;
+          createdAt: new Date(),
+        },
+      },
+    ]
+    return posts.find((post) => {
+      return post.id === id
     })
   }
 
   async findAll(page: number, limit: number): Promise<IPost[]> {
-    const hashedPassword = await hash("123", 8)
-    return [{
-      title: "titulo",
-      content: "content",
-      author: {
-        name: "Usuario",
-        email: "123",
-        password: hashedPassword,
-        isadmin: true,
-        posts: [],
-        createdAt: new Date()
-      }
-    }]
+    const hashedPassword = await hash('123', 8)
+    return [
+      {
+        title: 'titulo',
+        content: 'content',
+        author: {
+          name: 'Usuario',
+          email: '123',
+          password: hashedPassword,
+          isadmin: true,
+          posts: [],
+          createdAt: new Date(),
+        },
+      },
+    ]
   }
 
   async searchByWord(
@@ -70,38 +70,38 @@ export class PostRepository {
     limit: number,
     keyword: string,
   ): Promise<IPost[]> {
-    const hashedPassword = await hash("123", 8)
-    return [{
-      title: "titulo",
-      content: "content",
-      author: {
-        name: "Usuario",
-        email: "123",
-        password: hashedPassword,
-        isadmin: true,
-        posts: [],
-        createdAt: new Date()
-      }
-    }]
+    const hashedPassword = await hash('123', 8)
+    return [
+      {
+        title: 'titulo',
+        content: 'content',
+        author: {
+          name: 'Usuario',
+          email: '123',
+          password: hashedPassword,
+          isadmin: true,
+          posts: [],
+          createdAt: new Date(),
+        },
+      },
+    ]
   }
 
   async update(post: IPost): Promise<IPost> {
-    const hashedPassword = await hash("123", 8)
+    const hashedPassword = await hash('123', 8)
     return {
-      title: "titulo",
-      content: "content",
+      title: 'titulo',
+      content: 'content',
       author: {
-        name: "Usuario",
-        email: "123",
+        name: 'Usuario',
+        email: '123',
         password: hashedPassword,
         isadmin: true,
         posts: [],
-        createdAt: new Date()
-      }
+        createdAt: new Date(),
+      },
     }
   }
 
-  async delete(id: string): Promise<void> {
-    return;
-  }
+  async delete(id: string): Promise<void> {}
 }
